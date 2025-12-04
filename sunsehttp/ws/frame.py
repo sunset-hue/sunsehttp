455d1f3dc5ec7baf13bed95915dbede19ddb40d9
sunset-hue <sunsetree@proton.me>
2025-12-03 16:10:46 -0800

    going to figure out how to parse frames from ws

--- sunsehttp/ws/frame.py
+++ sunsehttp/ws/frame.py
@@ -16,6 +16,10 @@ class ClosureCodes(enum.Enum):
     PROTOCOL_ERROR = 1002
     UNACCEPTABLE = 1003
     NOT_CONSISTENT_DATA = 1007
+    POLICY_VIOLATION = 1008
+    MESSAGE_TOO_BIG = 1009
+    MISSING_EXTENSION = 1010
+    UNEXPECTED = 1011
 
 
 class SocketFrame:
@@ -24,6 +28,14 @@ class SocketFrame:
     def __init__(self, frame: bytes):
         self.parse = frame
         self.final: bool = False
+        """Whether this message was the final message sent by the websocket server."""
         self.opcode: int = 0
+        """The opcode of this message."""
+        self.masked: bool = False
+        """Whether this frame's data is masked."""
+        self.mask: bytes = b""
+        """The mask to unmask the data."""
+        self.data: bytes = b""
 
-    def a(): ...
+    def parser(self):
+        """parses bytes and returns"""
